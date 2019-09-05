9aae3c43fbea86553ce0a4a32aa77e1566bebbaf
Mehran Naghizadeh <tarhineh@gmail.com>
2019-09-05 14:52:56 +0200

    Generate the plugin scaffold

---
+++ app/components/editor-plugins/action-list-card.js
@@ -0,0 +1,1 @@
+export { default } from '@lblod/ember-rdfa-editor-action-list-plugin/components/editor-plugins/action-list-card';
---
+++ app/services/rdfa-editor-action-list-plugin.js
@@ -0,0 +1,1 @@
+export { default } from '@lblod/ember-rdfa-editor-action-list-plugin/services/rdfa-editor-action-list-plugin';
