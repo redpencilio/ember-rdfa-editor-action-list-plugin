d6c99a8e86ceb76eacf977cf78495e73c7b35032
Mehran Naghizadeh <tarhineh@gmail.com>
2019-09-16 17:59:16 +0200

    Implement UI for creation, selection and removal

--- addon/components/editor-plugins/action-list-card.js
+++ addon/components/editor-plugins/action-list-card.js
@@ -4,6 +4,7 @@ import layout from '../../templates/components/editor-plugins/action-list-card';
 import { inject as service } from '@ember/service';
 import { A }  from '@ember/array';
 import { computed }  from '@ember/object';
+import EmberObject from '@ember/object';
 
 /**
 * Card displaying a hint of the Date plugin
@@ -15,7 +16,7 @@ import { computed }  from '@ember/object';
 export default Component.extend({
   layout,
   store: service(),
-  actionItems: A([]),
+  selectedActions: A([]),
 
   /**
    * Region on which the card applies
@@ -49,31 +50,103 @@ export default Component.extend({
   */
   hintsRegistry: reads('info.hintsRegistry'),
 
+  canCommit: computed('status.{isCreating,status.isEditing,isSelecting}', 'selectedActions.[]', function(){
+    return this.status.isEditing && this.selectedActions.length;
+  }),
+
+  innerHTML: computed('selectedActions.[]', function(){
+    const list = this.selectedActions.map (action => `
+      <li property="notable:AgendaItemProceedingsActionItem"
+          typeof="pdo:ActionItem" >
+        [<span property="notable:actionItemOwner"
+              typeof="org:Membership">
+          <span property="org:member"
+                resource="http://data.notable.redpencil.io/persons/${action.get('owner.member.id')}"
+                typeof="foaf:Person">
+            <span property="foaf:firstName">${action.get('owner.member.firstname')}</span>
+            <span> </span>
+            <span property="foaf:lastName">${action.get('owner.member.lastname')}</span>
+          </span>
+        </span>]
+        <span property="pdo:hasDescription">${action.description}</span>
+      </li>`);
+
+    return `
+      <div class="text-gray-500 italic">
+        <i class="edit icon"></i> Edit the list
+      </div>
+      <ol about="/agenda/item-1"
+          property="dc:subject"
+          resource="agenda/item-1/proceedings"
+          typeof="notable:AgendaItemProceedings">
+        ${list.join('')}
+      </ol>`;
+  }),
+
+  nonSelectedActions: computed('allActions.[]', 'selectedActions.[]', function() {
+    return this.allActions.filter(action => ! this.selectedActions.includes(action));
+  }),
+
   async init() {
     this._super(...arguments);
+
+    this.set('status', EmberObject.create({
+      isAdding: false,
+      isCreating: false,
+      isEditing: true,
+    }));
+
     this.set('people', await this.store.findAll('person'));
     this.set('roles', await this.store.findAll('role'));
     this.set('memberships', (await this.store.findAll('membership')).sortBy('firstname'));
+    this.set('allActions', A([]));
+    this.set('selectedActions', A([]));
+
+    (await this.store.query('action-item', {
+      include: 'owner.member'
+    })).forEach(item => this.allActions.pushObject(item));
+  },
+
+  setStatus (status) {
+    this.status.set('isCreating', status === 'isCreating');
+    this.status.set('isEditing', status === 'isEditing');
+    this.status.set('isSelecting', status === 'isSelecting');
   },
 
   actions: {
-    async addActionItem(){
+    async createAction(){
+      // Create a new action
       const item = this.store.createRecord('actionItem', {
         description: this.actionItemDescription,
         owner: this.selectedMembership
       });
 
-      this.actionItems.pushObject(item);
+      // Push the newly created action to the list of selected actions without saving
+      this.selectedActions.pushObject(item);
 
-      this.set('isAdding', false);
+      // Show the list of selected actions
+      this.setStatus('isEditing');
 
+      // Reset the input fields to their default values
       this.set('actionItemDescription', '');
       this.set('selectedPerson', null);
       this.set('selectedRole', null);
     },
 
-    commit(){
-      debugger;
+    async commit(){
+      // First save all newly created actions
+      await this.selectedActions.filter(item => ! item.id).forEach(async item => await item.save());
+
+      const mappedLocation = this.get('hintsRegistry').updateLocationToCurrentIndex(this.get('hrId'), this.get('location'));
+      this.get('hintsRegistry').removeHintsAtLocation(mappedLocation, this.get('hrId'), 'editor-plugins/attendee-list-card');
+
+      const selection = this.editor.selectContext(mappedLocation, { typeof: this.info.typeof });
+      this.editor.update(selection, {
+        set: {
+          innerHTML: this.innerHTML
+        }
+      });
+      this.hintsRegistry.removeHintsAtLocation(this.location, this.hrId, this.who);
     },
 
     async createMembership(){
@@ -84,9 +157,14 @@ export default Component.extend({
       await membership.save();
     },
 
-    removeActionItem (item) {
-      this.actionItems.removeObject(item);
-      item.destroyRecord();
+    selectAction (action) {
+      this.selectedActions.pushObject(action);
+      this.setStatus('isEditing');
+    },
+
+    async deleteAction (action) {
+      this.allActions.removeObject(action);
+      await action.destroyRecord();
     },
 
     setOwner (event) {
@@ -99,6 +177,10 @@ export default Component.extend({
 
     setRole (event) {
       this.set('selectedRole', this.roles.find(role => role.id == event.currentTarget.value));
+    },
+
+    setStatus (status) {
+      this.setStatus(status);
     }
   }
 });
